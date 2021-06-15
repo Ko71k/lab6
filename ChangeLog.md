a18efaa92b6c4af13561479b4fa76b12023456b0
ko71k <rassmagin.wolf@mail.ru>
2021-06-15 18:41:19 +0300

    all

--- ChangeLog.md
+++
@@ -1,2 +0,0 @@
-* Tue Jun 15 2021 ko71k <rassmagin.wolf@mail.ru> 0.1.0.0
-- Initial RPM release
--- hello_world_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake
+++ hello_world_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake
@@ -1,22 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/rogopl/travis/build/Rogopl/lab04/hello_world_application/formatter_ex_lib/formatter_ex.cpp" "/home/rogopl/travis/build/Rogopl/lab04/hello_world_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "formatter_ex_lib"
-  "formatter_ex_lib/formatter_lib"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/supsun/Desktop/workspace/projects/lab06/hello_world_application/formatter_ex_lib/formatter_ex.cpp" "formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o" "gcc" "formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o.d"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/rogopl/travis/build/Rogopl/lab04/hello_world_application/formatter_ex_lib/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  "/home/supsun/Desktop/workspace/projects/lab06/hello_world_application/formatter_ex_lib/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
